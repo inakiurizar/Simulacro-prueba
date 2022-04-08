6faaaa354e398b81340c258616dc1028d6c621f5
triruz <101410794+triruz@users.noreply.github.com>
2022-04-08 11:34:32 -0300

    EJ01 - EJ02

--- Assets/Scripts/EJ01.cs
+++ Assets/Scripts/EJ01.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class EJ01 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int val1;
+    public int val2;
+
+    //Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
     void Start()
     {
-        
+
+        if (val1 == val2)
+        {
+            Debug.Log("Son iguales");
+        }
+        else
+        {
+            Debug.Log("No son iguales");
+        }
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/EJ02.cs
+++ Assets/Scripts/EJ02.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class EJ02 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.
+    //Comunicar si la suma de los mismos supera o no el monto de dinero disponible.Luego indicar cuánto dinero sobra o falta
+
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    public float montoDis;
+
     void Start()
     {
+        float sumaPrecios = precio1 + precio2 + precio3;
+
+        if(sumaPrecios > montoDis)
+        {
+            Debug.Log("La suma de los precios supera el monto disponible");
+            Debug.Log($"Faltan {sumaPrecios -  montoDis}");
+        } 
+        else
+        {
+            Debug.Log("Con el monto disponible, se pueden comprar los tres productos");
+            Debug.Log($"Sobran {montoDis - sumaPrecios}");
+        }
         
+
     }
+
+    
 
     // Update is called once per frame
     void Update()
